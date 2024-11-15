4f4cc7a9d6760dd67165944db5b97afbbfe73355
LucasMarisco <lucasmarisco27@gmail.com>
2024-11-15 18:06:28 -0300

    Update testbench.vhd
    semi funcional, usei o wait until pra fazer os sistema esperar pelo done, aí resolveu os problemas, mas tá tendo problemas no começo(que o done sobe e desce rapidinho) e nos final lá, aí tem quer ver pq

--- Grupo-3/sad-v3/testbench.vhd
+++ Grupo-3/sad-v3/testbench.vhd
@@ -13,16 +13,19 @@ architecture tb of testbench is
 CONSTANT B : natural := 32;
 CONSTANT N : natural := 16;
 CONSTANT P : natural := 4;
-signal clk,enable: std_logic := '0';
-signal reset: std_logic := '1';
+signal clk,reset: std_logic := '0';
+signal enable: std_logic := '1';
 signal sample_ori,sample_can: std_logic_vector(B-1 DOWNTO 0);
 signal read_mem,done: std_logic;
 signal address: std_logic_vector(3 downto 0);
 signal sad_value: std_logic_vector(13 downto 0);
+signal finished: std_logic := '0';
 
-CONSTANT passo : TIME := 540 ns; -- tempo para a sad ler 1 dos 16 valores da memoria
+CONSTANT passo : TIME := 530 ns; -- tempo para a sad ler 1 dos 16 valores da memoria
 -- 540 é o tempo para terminar 1 teste
 CONSTANT period : TIME := 10 ns; -- tempo do clock
+-- tempo no nosso clock = 104.6 Mhz = 9.5547487101089 ns
+--duplicamos para ter garantia de tempo logo 20 ns.
 
 begin
 
@@ -30,14 +33,14 @@ begin
   DUV: entity work.SAD_V3
     port map(clk, enable,reset,sample_ori,sample_can,read_mem,address,sad_value,done);
 
-    clk <= not clk after period/2;
-    reset <= '0' after 10 ns;
-    enable <= '1' after 10 ns;
+    clk <= not clk after period/2 when finished /= '1' else '0';
+    --reset <= '0' after 10 ns;
+    --enable <= '1' after 10 ns;
 
     stim: process is
        file arquivo_de_estimulos : text open read_mode is "../../estimulos.dat";
       -- vou organizar os dados assim:
-      -- Valores de A,B e saida vão tá assim -> 00000000000000000000000000000000 11111111111111111111111111111111  Valor esperado      
+      -- Valores de A,B e saida vão tá assim -> 00000000000000000000000000000000 11111111111111111111111111111111  11111111000000     
        variable linha_de_estimulos: line; --Todos os valores que precisamos tão em 1 linha
        variable espaco: character;
        variable valor_de_entrada_A: bit_vector(31 downto 0);
@@ -61,7 +64,8 @@ begin
           read(linha_de_estimulos, espaco); -- pula um espaço em branco
 
           read(linha_de_estimulos, valor_de_saida); -- lê o valor esperado
-          wait for passo;
+          wait until done = '1';
+          wait for 10 ns;
             assert (sad_value = to_stdlogicvector(valor_de_saida))
               report
                 "Falha na simulacao. "
@@ -69,6 +73,7 @@ begin
           end loop;
                     
           wait for passo;
+          finished <= '1';
           assert false report "Test done." severity note;
           wait;
           end process;
